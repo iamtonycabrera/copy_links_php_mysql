8f56e9828006669a374e47dae7dfafbd78117691
Antonio Cabrera <iamtonycabrera@gmail.com>
2023-05-15 14:14:31 +0200

    Implemented add categories methods

--- agregar_categoria.php
+++ agregar_categoria.php
@@ -1,4 +1,39 @@
-<?php include ("includes/header.php"); ?>
+<?php
+
+    include ("includes/header.php");
+    
+    if(isset($_POST['crearCategoria'])){
+        // Obtenemos los valores
+        $nombreCat = $_POST['nombre_categoria'];
+
+        if(empty($nombreCat) || $nombreCat == ""){
+            $error = "Error, algunos campos obligatorios estan vacios";
+            header("Location: agregar_categoria.php?error=" . urlencode($error));
+        } else {
+            $fechaActual = date("Y-m-d");
+
+            // Si entra por aqui es porque se puede crear
+
+            $query = "INSERT INTO categorias (nombre, fecha_creacion) VALUES(:nombre, :fecha_creacion)";
+
+            $stmt = $conectar->prepare($query);
+
+            $stmt->bindParam(':nombre', $nombreCat, PDO::PARAM_STR);
+            $stmt->bindParam(':fecha_creacion', $fechaActual, PDO::PARAM_STR);
+
+            $resultado = $stmt->execute();
+
+            if($resultado){
+                $mensaje = "Categoria creada";
+                header("Location: index.php?mensaje=" . urlencode($mensaje));
+                exit();
+            } else {
+                $error = "Error, no se pudo crear la categoria";
+                header("Location: index.php?error=" . urlencode($error));
+            }
+        }
+    }
+?>
 
 <div class="container mt-3">
 <div class="row">
@@ -15,7 +50,7 @@
 <div class="container card">
     <div class="row">
         <div class="col-6 offset-3">
-        <form method="POST" action="">
+        <form method="POST" action="<?php $_SERVER['PHP_SELF']; ?>">
             <div class="mb-3">
                 <label for="nombre" class="form-label">Nombre categoría:</label>
                 <input type="text" class="form-control" name="nombre_categoria" placeholder="Ingresa el nombre de la categoría">                
